c9bb722a06a6ac64f7de82cc2bbb953834f79885
john.chen <ban77323@hotmail.com>
2018-01-09 09:17:13 +0000

    parse image from folder and save detection result to image

--- run_tf.py
+++ run_tf.py
@@ -6,8 +6,12 @@ import tarfile
 import tensorflow as tf
 import zipfile
 
+import matplotlib
+
 from collections import defaultdict
 from io import StringIO
+
+matplotlib.use('Agg')
 from matplotlib import pyplot as plt
 from PIL import Image
 
@@ -86,7 +90,7 @@ def load_image_into_numpy_array(image):
 # image1.jpg
 # image2.jpg
 # If you want to test the code with your images, just add path to the images to the TEST_IMAGE_PATHS.
-PATH_TO_TEST_IMAGES_DIR = '../test_images'
+PATH_TO_TEST_IMAGES_DIR = '../tmp_img'
 #TEST_IMAGE_PATHS = [ os.path.join(PATH_TO_TEST_IMAGES_DIR, 'image{}.jpg'.format(i)) for i in range(1, 3) ]
 TEST_IMAGE_PATHS = []
 
@@ -95,32 +99,39 @@ for file in glob.glob('{}/*.jpg'.format(PATH_TO_TEST_IMAGES_DIR)):
     TEST_IMAGE_PATHS.append(file)
 
 
-"""
 # Size, in inches, of the output images.
 IMAGE_SIZE = (12, 8)
 
 with detection_graph.as_default():
   with tf.Session(graph=detection_graph) as sess:
+    
     # Definite input and output Tensors for detection_graph
     image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
     # Each box represents a part of the image where a particular object was detected.
     detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
     # Each score represent how level of confidence for each of the objects.
     # Score is shown on the result image, together with the class label.
     detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
     detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
     num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
     for image_path in TEST_IMAGE_PATHS:
       image = Image.open(image_path)
+
       # the array based representation of the image will be used later in order to prepare the
       # result image with boxes and labels on it.
       image_np = load_image_into_numpy_array(image)
+
       # Expand dimensions since the model expects images to have shape: [1, None, None, 3]
       image_np_expanded = np.expand_dims(image_np, axis=0)
+
       # Actual detection.
       (boxes, scores, classes, num) = sess.run(
           [detection_boxes, detection_scores, detection_classes, num_detections],
           feed_dict={image_tensor: image_np_expanded})
+
       # Visualization of the results of a detection.
       vis_util.visualize_boxes_and_labels_on_image_array(
           image_np,
@@ -130,6 +141,8 @@ with detection_graph.as_default():
           category_index,
           use_normalized_coordinates=True,
           line_thickness=8)
+      
+
       plt.figure(figsize=IMAGE_SIZE)
-      plt.imshow(image_np)
-"""
+      #plt.imshow(image_np)
+      plt.imsave('aaa.png', image_np)
